5e665a2c4aba29e5741dc86e8f05562df4a025c2
Brett Earle <brettearle@gmail.com>
2024-09-21 06:58:57 +0930

    flags and sorting command itemname

--- main.go
+++ main.go
@@ -3,32 +3,75 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
+type ItemList []Item
 
-type item struct{
+type Item struct {
 	done bool
 	desc string
 }
-func newItem(desc string)(*item, error){
-	i := &item{
+
+func NewItem(desc string) *Item {
+	i := &Item{
 		done: false,
 		desc: desc,
 	}
-	return i, nil
+	return i
+}
+
+func (i *Item) Create() {
+	fmt.Printf("Created: %+v\n", i)
+}
+
+func (i *Item) TickUntick() {
+	if i.done {
+		i.done = false
+	}
+	i.done = true
+	fmt.Printf("Ticked: %+v\n", i)
+}
+
+func (i *Item) Print() {
+	fmt.Printf("Current Item: %+v\n", i)
 }
 
 func main() {
 	//Flag Decleration
-	newFlag := flag.String("n", "Item Description", "New Item: Value is Item Description")
-	flag.Parse()
+	itemFlags := flag.NewFlagSet("Todo items", flag.ContinueOnError)
+	newFlag := itemFlags.Bool("n", false, "New Item")
+	tickFlag := itemFlags.Bool("t", false, "completes an item")
+	listFlag := itemFlags.Bool("ls", false, "list all items")
+	fmt.Println(*listFlag)
+	//Commands
+	commands := os.Args
+	//TODO sort out if just flags passed in
+	if len(commands) > 1 {
+		switch commands[1] {
+		//cases for commands go here 
+		default:
+			itemFlags.Parse(commands[2:])
+		}
+	} else {
+		fmt.Println("Need this structure for aswan command")
+		fmt.Println("aswan 'item name' -flag -fl.....")
+		return
+	}
+	//-- End Flag Decleration --
+	//Arguments
+	itemArg := commands[1]
+	//-- End Args --
 
 	//Exploration
-	fmt.Printf("newFlag: %s", *newFlag )
-	i,err := newItem("tested")
-	if (err != nil) {
-		fmt.Println("Item broke")
+	i := NewItem(itemArg)
+	if *tickFlag {
+		i.TickUntick()
+		i.Print()
 	}
-	fmt.Printf("item: %v", i)
+	if *newFlag {
+		ni := NewItem(itemArg)
+		ni.Create()
+		ni.Print()
+	}
 }
-
