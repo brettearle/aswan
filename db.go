065bbbd85f4e0dd4258f93c2e129036e6b4960e8
Brett Earle <brettearle@gmail.com>
2024-09-22 10:43:05 +0930

    create todo, start of getAll

---
+++ db.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+var db *sql.DB
+
+type aswanDB struct {
+	path string
+	db *sql.DB
+}
+func (db *aswanDB) createTodo(todo *item) (sql.Result, error) {
+	res, err := db.db.ExecContext(context.Background(), `INSERT INTO todo (desc, done) VALUES (?,?);`, todo.done, todo.desc,)
+	if err != nil {
+		fmt.Printf("sql Error: %v\n", err)
+		return res, errors.New("could not create todo")
+	}
+	return res, nil
+}
+func (db *aswanDB) getAllTodos() (*itemList, error) {
+	rows, err := db.db.Query("SELECT * FROM todo")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return nil, err
+	}
+	defer rows.Close()
+	var res itemList
+	for rows.Next() {
+		var item item
+		// TODO rows scan needs error handling
+		rows.Scan(&item.desc, &item.done)
+		res = append(res, &item)
+	}
+	return &res, nil
+}
+
+func newAswanDB(path string, DB *sql.DB) *aswanDB {
+	db := &aswanDB{
+		path: path,
+		db: DB,
+	}
+	return db
+}
+
+func dbInit(path string) (*aswanDB, error) {
+	var err error
+	db, err = sql.Open("sqlite", path)
+	if err != nil {
+		fmt.Printf("Failed to open db %v\n", err)
+		return nil,err
+	}
+	_, err = db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS todo (
+			id INTEGER PRIMARY KEY AUTOINCREMENT, 
+			desc TEXT NOT NULL, 
+			done BOOLEAN NOT NULL 
+		)`,
+	)
+	if err != nil {
+		fmt.Printf("Failed to create %v\n", err)
+		return nil,err
+	}
+	r := newAswanDB(path, db)	
+	return r, nil
+}
--- main.go
+++ main.go
@@ -6,43 +6,45 @@ import (
 	"fmt"
 	"os"
 	"strings"
-
-	"github.com/brettearle/aswan/db"
 )
 
-type ItemList []*Item
+type itemList []*item
 
-type Item struct {
+type item struct {
 	done bool
 	desc string
 }
 
-func (t Item) String() string {
+func (t item) String() string {
 	return fmt.Sprintf("\n{%v %v}\n", t.done, t.desc)
 }
 
 // helpers
-func NewItem(desc string) *Item {
-	i := &Item{
+func newItem(desc string) *item {
+	i := &item{
 		done: false,
 		desc: desc,
 	}
 	return i
 }
-func GetItem(desc string, iL ItemList) (*Item, error) {
+func getItem(desc string, iL itemList) (*item, error) {
 	for _, item := range iL {
 		if item.desc == desc {
 			return item, nil
 		}
 	}
-	return nil, errors.New("Item Not In List")
+	return nil, errors.New("item not in list")
 }
 
-func (i *Item) Create() {
-	fmt.Printf("Created: %+v\n", i)
+func (i *item) create(db *aswanDB) {
+	res, err := db.createTodo(i)	
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	fmt.Printf("Created: %+v\n with res: %v\n", i, res)
 }
 
-func (i *Item) TickUntick() {
+func (i *item) tickUntick() {
 	if i.done {
 		i.done = false
 	} else {
@@ -51,17 +53,21 @@ func (i *Item) TickUntick() {
 	fmt.Printf("Ticked: %+v\n", i)
 }
 
-func (i *Item) Print() {
+func (i *item) print() {
 	fmt.Printf("Current Item: %+v\n", i)
 }
 
 func main() {
+	//DB Initialization
+	testDB, err := dbInit("./test.db")
+	if err != nil {
+		fmt.Println("failed to init DB")
+	}
 	//Flag Decleration
 	itemFlags := flag.NewFlagSet("Todo items", flag.ContinueOnError)
 	newFlag := itemFlags.Bool("n", false, "New Item")
 	tickFlag := itemFlags.Bool("t", false, "Completes an item")
 	listFlag := itemFlags.Bool("ls", false, "List items")
-	fmt.Println(*listFlag)
 	//Commands
 	commands := os.Args
 	_, _, flagFirst := strings.Cut(commands[1], "-")
@@ -79,6 +85,8 @@ func main() {
 		//cases for commands go here
 		case "help":
 			fmt.Println("help not implemented")
+		case "dbPath":
+			fmt.Println("path to DB")
 		case "timer":
 			fmt.Println("timer not yet implemented")
 		default:
@@ -92,35 +100,25 @@ func main() {
 	//-- End Args --
 
 	//Exploration
-	var testList ItemList
-	testList = append(testList, NewItem(itemDesc))
 	if *tickFlag {
-		i, err := GetItem(itemDesc, testList)
-		if err != nil {
-			i = NewItem(itemDesc)
-			testList = append(testList, i)
-		}
-		i.TickUntick()
-		i.Print()
+		i := newItem(itemDesc)
+		i.tickUntick()
+		i.print()
 	}
 	if *newFlag {
-		ni, err := GetItem(itemDesc, testList)
-		if err != nil {
-			ni = NewItem(itemDesc)
-			testList = append(testList, ni)
-		}
-		ni.TickUntick()
-		ni.Create()
-		ni.Print()
+		ni := newItem(itemDesc)
+		ni.tickUntick()
+		ni.create(testDB)
+		ni.print()
 	}
 	if *listFlag {
+		res, err := testDB.getAllTodos()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		}
 		fmt.Println("All Items -")
-		fmt.Printf("%v\n", testList)
-	}
-	testDB, err := db.Init("./test.db")
-	if err != nil {
-		fmt.Println("failed to init DB")
+		fmt.Println(*res)
 	}
 	fmt.Printf("DB: %+v", testDB)
-
 }
+
