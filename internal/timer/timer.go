ed30460851650c4a9dbd7ef23d95b52be9ad209c
Brett Earle <brettearle@gmail.com>
2024-10-05 06:44:22 +0930

    fmt

--- internal/timer/timer.go
+++ internal/timer/timer.go
@@ -8,10 +8,10 @@ import (
 
 // TermPomoTimer is what is used to run pomodoro timer in terminal
 type TermPomoTimer struct {
-	Work int
-	Rest int
-	Rounds int
-	LongRest int
+	Work          int
+	Rest          int
+	Rounds        int
+	LongRest      int
 	CurrentTimers []*time.Timer
 }
 
@@ -32,11 +32,11 @@ func (t *TermPomoTimer) Reset()   {}
 func (t *TermPomoTimer) Set()     {}
 func (t *TermPomoTimer) Display() {}
 
-func NewTermPomoTimer(work int, rest int, rounds int, lngRest int ) *TermPomoTimer {
+func NewTermPomoTimer(work int, rest int, rounds int, lngRest int) *TermPomoTimer {
 	return &TermPomoTimer{
-		Work: work,
-		Rest: rest,
-		Rounds: rounds,
+		Work:     work,
+		Rest:     rest,
+		Rounds:   rounds,
 		LongRest: lngRest,
 	}
 }
--- internal/timer/timer_test.go
+++ internal/timer/timer_test.go
@@ -9,14 +9,14 @@ func TestTimer(t *testing.T) {
 	t.Run("Constructor should return TermPomoTimer", func(t *testing.T) {
 		got := NewTermPomoTimer(15, 5, 8, 15)
 		want := &TermPomoTimer{
-			Work: 15,
-			Rest: 5,
-			Rounds: 8,
+			Work:     15,
+			Rest:     5,
+			Rounds:   8,
 			LongRest: 15,
 		}
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v wanted %v", got, want)
 		}
-	})	
-	
+	})
+
 }
