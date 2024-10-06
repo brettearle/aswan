c5b0726bc6d9b4e09140b0b6abe2b445a8742705
Brett Earle <brettearle@gmail.com>
2024-10-07 06:53:01 +1030

    WIP: Timer methods

--- internal/timer/timer.go
+++ internal/timer/timer.go
@@ -3,18 +3,19 @@ package timer
 import (
 	"fmt"
 	"strconv"
-	"syscall"
 	"time"
 )
 
 type SetTimer struct {
 	job   string
+	start time.Time
 	timer *time.Timer
 }
 
 func NewSetTimer(j string, t *time.Timer) *SetTimer {
 	return &SetTimer{
 		job:   j,
+		start: time.Now(),
 		timer: t,
 	}
 }
@@ -29,8 +30,6 @@ type TermPomoTimer struct {
 }
 
 func (t *TermPomoTimer) Start() {
-	//maybe syscall aswan to start some other process or a instance of a timer server style
-	syscall.Exec("ls", []string{"ls"}, []string{"ls"})
 	workStr := strconv.FormatInt(int64(t.Work), 10)
 	duration, err := time.ParseDuration(workStr + "m")
 	if err != nil {
@@ -38,10 +37,11 @@ func (t *TermPomoTimer) Start() {
 	}
 
 	timer := time.NewTimer(duration)
+
 	res := NewSetTimer("work", timer)
 	t.CurrentTimers = append(t.CurrentTimers, res)
-	fmt.Printf("Sleep is done: %v", t)
 }
+
 func (t *TermPomoTimer) KillAllTimers() {
 	if len(t.CurrentTimers) > 1 {
 		for _, ti := range t.CurrentTimers {
@@ -50,11 +50,24 @@ func (t *TermPomoTimer) KillAllTimers() {
 	}
 }
 
-func (t *TermPomoTimer) Stop() {
-
+func (t *TermPomoTimer) Stop(jb string) {
+	var tm *SetTimer
+	for _, ct := range t.CurrentTimers {
+		if ct.job == jb {
+			tm = ct
+		}
+	}
+	tm.timer.Stop()
 }
 
-func (t *TermPomoTimer) Display() {}
+func (t *TermPomoTimer) Display() {
+	for _, ct := range t.CurrentTimers {
+		diff := time.Since(ct.start)
+		diffStr := diff.String()
+		fmt.Printf("Time: %v", diffStr)
+	}
+
+}
 
 func NewTermPomoTimer(work int, rest int, rounds int, lngRest int) *TermPomoTimer {
 	return &TermPomoTimer{
